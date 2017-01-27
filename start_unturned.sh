075b66c8069dad51c5ab0cf6ec6ceadd17c34d56
DefCon42 <klrehm123@gmail.com>
2017-01-27 17:11:59 -0500

    formatting

--- start_unturned.sh
+++ start_unturned.sh
@@ -12,16 +12,16 @@ SCREEN_NAMES=(
 ) #Names of your server
 ROCKET_START_DIR="" #The directory of the start.sh program installed when Rocketmod is first installed.
 count=0
-		while [ "x${SCREEN_NAME[count]}" != "x" ]
-		do
-			count=$(( $count + 1 ))
-			if ! tmux ls | grep -q {SCREEN_NAME[count]}; then 
-				echo "Unturned server successfully started"
-				unset TMUX #We need to do this in order to be able to start the unturned server from another session later.
-				tmux new -s {SCREEN_NAME[count]} -d
-				tmux send-keys -t {SCREEN_NAME[count]} "$ROCKET_START_DIR {SERVER_NAMES[count]}" C-m
-			else
-				echo "Server already started"
-			fi
-		done	
+while [ "x${SCREEN_NAME[count]}" != "x" ]		
+do
+	count=$(( $count + 1 ))
+	if ! tmux ls | grep -q {SCREEN_NAME[count]}; then 
+		echo "Unturned server successfully started"
+		unset TMUX #We need to do this in order to be able to start the unturned server from another session later.
+		tmux new -s {SCREEN_NAME[count]} -d
+		tmux send-keys -t {SCREEN_NAME[count]} "$ROCKET_START_DIR {SERVER_NAMES[count]}" C-m
+	else
+		echo "Server already started"
+	fi
+done	
 
